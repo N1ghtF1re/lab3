91d3a83dd7d5c53ee55351357ded0509710d5d21
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2017-10-09 23:04:14 +0300

    Added functional: Calculation of the values for f2 (Not ready yet)

--- lab3.dpr
+++ lab3.dpr
@@ -5,44 +5,62 @@ program lab3;
 uses
   SysUtils;
 
-var x, f1,f2:real;
-N,i,a,b:integer;
-stringx:string;
-first:boolean;
+var x,dx,f1,f2,z,e,epsilon,prev,f2frac,ecurrent:real;
+N:integer;
+i,ExecutionsNum,CurrentNum,k:Byte;
 begin
-//x:=1.112131;
-
-N:=123;
-first:=false;
-repeat
-writeln('Enter the value of x. To exit, enter "N" or "None" or "Exit"');
-readln(stringx);
-Val(stringx,x,b);
-if(b=0) then
-begin
-  if(first=false) then
-  begin
-    writeln('------------------------------------------------------------------------');
-    writeln('|       |        |      e=10^2     |      e=10^3     |      e=10^4     |');
-    writeln('|   x   |  f1(x) |------------------------------------------------------');
-    writeln('|       |        |  f2(x) |   N    |  f2(x) |   N    |  f2(x) |   N    |');
-    writeln('------------------------------------------------------------------------');
-   end;
-f1:=0;
-f2:=0;
-
-if (first=true) then
+  x:=-0.6; // initial values
+  ExecutionsNum:= 20; // number of function values to be found
+  dx:= 0.05; // step
+  CurrentNum:=1;
+  e:=dx/1000;
   writeln('------------------------------------------------------------------------');
-first:=true;
+  writeln('|       |        |      e=10^2     |      e=10^3     |      e=10^4     |');
+  writeln('|   x   |  f1(x) |------------------------------------------------------');
+  writeln('|       |        |  f2(x) |   N    |  f2(x) |   N    |  f2(x) |   N    |');
+  writeln('------------------------------------------------------------------------');
 
-write('|',x:7:3,'|',f1:8:3,'|');
-for i:= 1 to 3 do begin
-write(f2:8:3,'|',N:8,'|');
-end;
-writeln;
-writeln('------------------------------------------------------------------------');
-end;
-until(b>0);
+  while(CurrentNum <= ExecutionsNum) do
+  begin
 
+    f2:=0;
+
+    z:=Sqrt(1+sqr(x));
+
+    f1:=Ln(x+z)/z;
+    write('|',x:7:2,'|',f1:8:3,'|');
+
+    epsilon:=1E-2;
+    N:=0;
+    f2:=x;
+    ecurrent:=1;
+    k:=1;
+    while(ecurrent>=1E-4) do
+    begin
+
+      f2frac:=((-1)*4*k*k*x*x)/((2*k+1)*2*k);
+      prev:=f2;
+      f2:=f2+f2*f2frac;
+      ecurrent:=Abs(f2-prev);
+      if (prev-f2 = 0) then Write('     The function does not change')
+      else begin
+
+        if(ecurrent < epsilon) then
+        begin
+          //Writeln('f2 = ', f2, 'epsilon = ', ecurrent, '/', epsilon);
+          write(f2:8:3,'|',k:8,'|');
+          epsilon:=epsilon/10;
+        end;
+      end;
+      inc(k);
+    end;
+    writeln;
+    writeln('------------------------------------------------------------------------');
+    x:=x+dx;
+    Inc(CurrentNum);
+  end;
+  Writeln;
+  Writeln;
+  Writeln('Press Enter to exit.');
+  Readln;
 end.
- 
